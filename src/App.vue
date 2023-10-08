598a992478424a5fdf5e29ea710fa4e31f32e07f
Daniel151505 <daniel150520011@gmail.com>
2023-10-08 17:57:04 -0500

    Agregando select para las monedas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script setup>
+import { ref } from "vue";
 
+const monedas = ref([
+  { codigo: "USD", texto: "Dolar de Estados Unidos" },
+  { codigo: "MXN", texto: "Peso Mexicano" },
+  { codigo: "EUR", texto: "Euro" },
+  { codigo: "GBP", texto: "Libra Esterlina" },
+]);
 </script>
 
 <template>
+  <div class="contenedor">
+    <div class="titulo">Cotizador de <span>Criptomonedas</span></div>
+    <div class="contenido">
+      <div class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="">-- Selecciona --</option>
+            <option v-for="moneda in monedas" :value="moneda.codigo">
+              {{ moneda.texto }}
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
